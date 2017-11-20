54de153904f5da82a291677301b12df625f2fc8a
Daniel Probst <daenuprobst@gmail.com>
2017-11-20 16:52:29 +0100

    Fixed several bugs.

--- test/force.js
+++ test/force.js
@@ -6,5 +6,6 @@ let force = [
     'CC(N)C12CC3CC(CC(C3)C1)C2',
     '[H][C@@]1(C)CCC2=CC(C[C@@](C)(N)C(=O)OCC3=CC(CC1)=NC(=C3)N(C)S(C)(=O)=O)=CC=C2',
     'NC1=CC=C2C(=O)NC(=O)C3=CC=CC1=C23',
-    'CC1=C(C2=CC3=NC(=CC4=C(C(=C([N-]4)C=C5C(=C(C(=N5)C=C1[N-]2)C)CCC(=O)O)CCC(=O)O)C)C(=C3C)C=C)C=C'
+    'CC1=C(C2=CC3=NC(=CC4=C(C(=C([N-]4)C=C5C(=C(C(=N5)C=C1[N-]2)C)CCC(=O)O)CCC(=O)O)C)C(=C3C)C=C)C=C',
+    '[H][C@@]12CC3=CC=C(OC4=C5C(CC[N+](C)(C)[C@]5([H])CC5=CC(OC6=C(OC)C=C(CCN1C)C2=C6)=C(O)C=C5)=CC(OC)=C4O)C=C3'
 ]
