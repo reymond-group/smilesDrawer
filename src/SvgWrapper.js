c9bb8d3f5e374daee0fb8ab75f8f5ed561d8be8f
Daniel Probst <daenuprobst@gmail.com>
2022-04-02 20:01:44 +0200

    Removed debugging output

--- src/SvgWrapper.js
+++ src/SvgWrapper.js
@@ -596,7 +596,6 @@ class SvgWrapper {
 
     // Measure element name only, without charge or isotope
     let bbox = this.measureText(text[0][1]);
-    console.log(bbox);
     let textElem = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     textElem.setAttributeNS(null, 'class', 'element');
     let g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
@@ -666,7 +665,7 @@ class SvgWrapper {
     const ctx = element.getContext("2d");
     ctx.font = `${this.opts.fontSizeLarge}pt Helvetica, Arial, sans-serif`
     let textMetrics = ctx.measureText(text)
-    console.log(textMetrics);
+
     return {
       'width': textMetrics.width, // Math.abs(textMetrics.actualBoundingBoxLeft) + Math.abs(textMetrics.actualBoundingBoxRight),
       'height': (Math.abs(textMetrics.actualBoundingBoxAscent) + Math.abs(textMetrics.actualBoundingBoxAscent)) * 0.9
