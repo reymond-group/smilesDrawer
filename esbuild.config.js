7e13018168451414b061256ebb1fe68c4b12c997
Nekomeme <nekomemepi@gmail.com>
2025-06-02 10:56:53 +0100

    Update esbuild.config.js
    fix output filename

--- esbuild.config.js
+++ esbuild.config.js
@@ -2,10 +2,10 @@
 const esbuild = require('esbuild');
 
 esbuild.build({
-  entryPoints: ['app.jsx'], // Your main JavaScript file
+  entryPoints: ['app.jsx'], // main JavaScript file
   bundle: true,
   minify: true, // Enable minification
-  outfile: 'dist/bundle.js', // Specify the output file path
+  outfile: 'dist/bundle.min.js', // output file path
   sourcemap: true,
   target: 'es2017'
 }).catch(() => process.exit(1));
